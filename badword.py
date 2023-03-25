d632254711ee29734ea2fb2187fd982478920baa
NaderBessioud <79223546+NaderBessioud@users.noreply.github.com>
2023-03-25 13:18:07 +0100

    Update badword.py

--- badword.py
+++ badword.py
@@ -24,7 +24,7 @@ def profile():
     option=request.args.get('option')
     niveau=request.args.get('niveau')
     try:
-        connection = mysql.connector.connect(host='192.168.2.176',
+        connection = mysql.connector.connect(host='192.168.1.21',
                                          database='esprithub',
                                          user='root',
                                          password='guessitplease',
